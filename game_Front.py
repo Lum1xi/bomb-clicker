b09a4e6d05cac94051efc93d5f2c968822b5b797
Lum1xi <mrlumixi@gmail.com>
2025-01-15 17:03:31 +0200

    adsrf

--- game_Front.py
+++ game_Front.py
@@ -1,31 +1,32 @@
-
+import tkinter as tk
+from main import Bomb
 
 def front():
-    import tkinter as tk
-    from main import Bomb
-    bomb_setup = Bomb()
-    root = tk.Tk()
+    root = tk.Tk()  
     root.title("Bomb Game")
     root.geometry('600x600')
     root.iconbitmap("asset/bomb.ico")
 
-    label = tk.Label(text="Press [Enter] to start the game",
+    bomb_setup = Bomb(root)  
+
+    label = tk.Label(root, text="Press [Enter] to start the game",
                      font=("Comic Sans MS", 20))
 
-    fuse_label = tk.Label(text=f"Time: {bomb_setup.timer}",
+    fuse_label = tk.Label(root, text=f"Time: {bomb_setup.timer}",
                           font=("Comic Sans MS", 20))
 
-    score_label = tk.Label(text=f"Score: {str(bomb_setup.score)}",
+    score_label = tk.Label(root, text=f"Score: {str(bomb_setup.score)}",
                            font=("Comic Sans MS", 20))
 
-    bomb_label = tk.Label(image=bomb_setup.img[0])
-
+    bomb_label = tk.Label(root, image=bomb_setup.img[0])
 
     label.pack()
     fuse_label.pack()
     score_label.pack()
     bomb_label.pack()
 
-
     root.mainloop()
+
+
+
 front()
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import tkinter as tk
-from game_Front import front
-
 
 class Bomb:
-    def __init__(self):
+    def __init__(self, root):
         self.timer = 100
         self.score = 0
         self.press_return = True
-        self.img = [tk.PhotoImage(file="asset/1.gif"),
-                    tk.PhotoImage(file="asset/2.gif"),
-                    tk.PhotoImage(file="asset/3.gif"),
-                    tk.PhotoImage(file="asset/4.gif")]
-front()
+
+        self.img = [tk.PhotoImage(master=root, file="asset/1.gif"),
+                    tk.PhotoImage(master=root, file="asset/2.gif"),
+                    tk.PhotoImage(master=root, file="asset/3.gif"),
+                    tk.PhotoImage(master=root, file="asset/4.gif")]
